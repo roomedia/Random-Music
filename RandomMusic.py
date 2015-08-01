e8191f231bb2170ed5c1b112c1f12e6d457c14fc
roomedia <012403@naver.com>
2015-08-01 13:22:21 +0900

    Code Refactoring And Add Function

--- RandomMusic.py
+++ RandomMusic.py
@@ -1,74 +1,93 @@
 from PySide import QtGui
+from PySide.QtCore import QSize
+
 class Dialog(QtGui.QWidget):
     def __init__(self):
-        """Constructor"""
-        # super(Dialog, self).__init__()
         QtGui.QWidget.__init__(self)
+        self.initUI()
+        self.getPlaylist()
 
-        #create components
-        musicListDocument = QtGui.QTextEdit
-        b = QtGui.QTextDocument
-        c = QtGui.QTextBlock
-        d = QtGui.QTextCursor
-        musicPlayBtn = QtGui.QPushButton("Play Random Music")
+    def initUI( self ):
+        self.playlistBox = QtGui.QListWidget()
+        self.setPlaylistButton = QtGui.QPushButton( "Set Playlist!" )
+        self.boxLabel = QtGui.QLabel("곡 수: ")
+        self.numberOfMusicBox = QtGui.QLineEdit( "30", parent=None )
+        
+        self.setPlaylistButton.clicked.connect( self.setPath )
 
-        #connect components to the functions
-        musicPlayBtn.clicked.connect(self.openMp3)
+        hBox = QtGui.QHBoxLayout()
+        hBox.addWidget( self.setPlaylistButton )
+        hBox.addWidget( self.boxLabel )
+        hBox.addWidget( self.numberOfMusicBox )
 
-        layout = QtGui.QVBoxLayout()
-        #layout.addWidget(musicListFrame)
-        layout.addWidget(musicPlayBtn)
-        self.setLayout(layout)
-        self.setGeometry(100,100, 400, 300)
-        self.setWindowTitle("랜덤 뮤직")
-        self.dir = ""
-        self.length = 0
+        vBox = QtGui.QVBoxLayout()
+        vBox.addWidget( self.playlistBox )
+        vBox.addLayout( hBox )
 
-    def openDirDialog(self):
-        """Opens the Directory Dialog"""
-        self.dir = QtGui.QFileDialog.getExistingDirectory()
-        self.updateMusicList()
+        self.setLayout( vBox )
+        self.setWindowTitle( "랜덤 뮤직" )
+        self.setGeometry( 100, 100, 400, 300 )
 
-    def openMp3(self):
-        """Saves music lists and opens music player"""
-        self.openDirDialog()
+    def setPath( self ):
+        path = QtGui.QFileDialog.getExistingDirectory()
+        if not path == '':
+            self.setPlaylist( path )
 
-    def updateMusicList(self):
+    def setPlaylist( self, path ):
         import os
-        print(self.dir)
-        self.fileList = os.listdir(self.dir)
-        self.fileList = [item for item in self.fileList\
-                          if item[-4:] == '.mp3' or item[-4:] == '.wav']
-        self.musicList = []
-        self.length = len(self.fileList)
-        self.selectMusic()
+        fileList = [ item for item in os.listdir( path ) if os.path.isfile( path + "\\" + item ) ]
+        tempList = []
+        musicFormat = '.3gp.aiff.aac.alac.amr.wav.au.awb.dvf.flac.mmf.mp3.mpc.msv.ogg.opus.tta.vox.wma'
+        
+        import re
+        for item in fileList:
+            for i in range(-5, -3):
+                if musicFormat.find( item[i:] ) == -1:
+                    remove = True
+                else:
+                    remove = False
+            if not remove:
+                tempList.append( item )
 
-    def selectMusic(self):
-        import random
-        if self.length == 0:
-            self.openError()
+        fileNumber = len( tempList )
+        musicNumber = int( self.numberOfMusicBox.text() )
+        
+        if fileNumber == 0:
+            return getErrorMsg()
+
+        if fileNumber <= musicNumber:
+            scope = fileNumber
         else:
-            for i in range(30):
-                r = random.randrange(0,self.length)
-                self.musicList.insert(-1, self.fileList[r])
-                self.fileList.pop(r)
-                self.length-=1
-                if self.length == 0:
-                    break;
-        self.makeMusicList()
+            scope = musicNumber        
 
-    #if musicList == 0 -> print Error Message and go back front function
-    def openError(self):
-        error = QtGui.QErrorMessage()
-        error.showMessage("해당 폴더에 음악 파일이 없습니다!")
-        error.exec_()
-        self.openMp3()
+        import random
+        playlist = open( "playlist.pls", "w" )
+        for i in range( scope ):
+            if i != scope:
+                seed = random.randrange( 0, fileNumber - i )
+                music = tempList[ seed ]
+                playlist.write( "File"+str(i)+"="+path+"\\"+music+"\n" )
+                tempList.remove( music )
+            else:
+                musicList.append( tempList[0] )
+        playlist.close()
+        self.getPlaylist()
 
-    def makeMusicList(self):
-        f = open("Random Music.pls", mode="w")
-        for index in range(len(self.musicList)):
-            f.writelines("File"+str(index)+"="+self.dir+"\\"+self.musicList[index]+"\n")
-        f.close()
+    def getPlaylist( self ):
+        print( "I'm getPlaylist!" )
+        self.playlistBox.clear()
+        playlist = open( "playlist.pls", "r" )
+        text = playlist.readlines()
+        playlist.close()
+
+        size = QSize( 150, 16 )
+
+        import re
+        for item in text:
+            item = re.sub( "File\d*=\w:([\\\\]\w+)*[\\\\](.*)[.].+", r"\2", item )
+            listItem = QtGui.QListWidgetItem( item )
+            listItem.setSizeHint( size )
+            self.playlistBox.addItem( listItem )
 
 if __name__ == "__main__":
     app = QtGui.QApplication([])
